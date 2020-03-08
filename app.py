6fd300a301fb869681fc9578c7c6a2ad9b3833be
WENXU-codinglife <wenxuw@sfu.ca>
2020-03-07 22:36:39 -0800

    20200307

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, request, jsonify
-import data
+import datetime
+from pytz import timezone
+#import data
 
 app = Flask(__name__, static_folder='', template_folder='')
 
@@ -9,15 +11,16 @@ def index():
     return render_template('index.html')
 
 
-#@app.route('/lookup', methods=['POST'])
-#def lookup():
-#    aud, defi = data.extr(request.form)
-#    return aud, defi
-
-#@app.route('/serverRetrieve', methods=['POST'])
-#def serverRetrieve():
-#    ret = data.store_retrieve(request.form)
-#    return ret
+@app.route('/flashcard', methods=['POST'])
+def flashcard():
+    x = datetime.datetime.now()
+    datetime_obj_pacific = timezone('US/Pacific').localize(x)
+    today = "2"
+    for i in range(1,10):
+        today = today + str(datetime_obj_pacific)[i]
+    print(today)
+    print(request.form['keyword'])
+    return 'ok'
 
 if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000.
